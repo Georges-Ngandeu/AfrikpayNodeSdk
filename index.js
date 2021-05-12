db2187501a82fcfe50c6b47e85f726c68b61cc2b
georgesngandeu <georgesngandeu@gmail.com>
2021-05-12 09:32:03 +0100

    gixing bugs

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import { Ecommerce } from './src/Ecommerce.js';
+import { Airtime } from './src/Airtime.js';
+import { Bill } from './src/Bill.js';
+import { Money } from './src/Money.js';
+import { Account } from './src/Account.js';
 
-let ecommerce = new Ecommerce(
-    'AFC5308',
-    'b2b0c952269cd5c38903433759369ac7',
-    '',
-    'http://34.86.5.170:8086/api/ecommerce/collect/'
-);
 
-ecommerce.collect('mtn_mobilemoney_cm', '677777777', 350)
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+module.exports = {
+    Ecommerce,
+    Airtime,
+    Bill,
+    Money,
+    Account
+};
